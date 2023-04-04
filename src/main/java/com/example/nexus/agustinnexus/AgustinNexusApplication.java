5014250142009cc63bb6c86816068d75375c6311
DESKTOP-MHUR8U3\agust <agustin.munoz.afincor@gmail.com>
2023-04-04 11:22:18 +0200

    first commit

---
+++ src/main/java/com/example/nexus/agustinnexus/AgustinNexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexus.agustinnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgustinNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgustinNexusApplication.class, args);
+	}
+
+}
